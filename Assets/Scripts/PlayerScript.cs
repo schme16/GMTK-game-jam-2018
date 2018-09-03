848cd96c79e74117d25df7f21516e7cb0224d28b
Shane Gadsby <schme16@gmail.com>
2018-09-03 17:37:17 +1000

    Didn't really make it

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+
+	public float speed;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		float jump = Input.GetAxis("Jump");
+		float horizontal = Input.GetAxis("Horizontal");
+		Rigidbody2D rb = GetComponent<Rigidbody2D>();
+
+		rb.AddForce((transform.right * speed) * horizontal);
+
+	}
+}
